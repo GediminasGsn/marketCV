4453f616be0dce5d6e73461fe413f2675b7e5acd
GediminasGsn <gasiunasgediminas@gmail.com>
2024-04-19 17:46:26 +0300

    ***

--- src/test/java/dataScrape.java
+++ src/test/java/dataScrape.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class dataScrape {
 
@@ -19,5 +23,20 @@ public class dataScrape {
         driver.get(url);
         driver.manage().window().maximize();
 
+        //Loop through every page
+        int i = 0;
+        String url1 = "https://www.cvmarket.lt/darbo-skelbimai?op=search&search%5Bjob_salary%5D=3&search%5Blocations%5D%5B0%5D=134&search%5Bcategories%5D%5B0%5D=8&search%5Bkeyword%5D=&ga_track=homepage&start=" + i + "0";
+        driver.get(url1);
+        while (i <= 201){
+            i += 3;
+        }
+
+            //Wait
+            try {
+                Thread.sleep(5000); // Wait for 5 seconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        List<WebElement> h3Elements = driver.findElements(By.xpath("//article"));
     }
 }
