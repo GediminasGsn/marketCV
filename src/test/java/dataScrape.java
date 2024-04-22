19a14cb29d391d00ec3ce0c9e28aecce20299205
GediminasGsn <gasiunasgediminas@gmail.com>
2024-04-22 18:48:41 +0300

    Final

--- src/test/java/dataScrape.java
+++ src/test/java/dataScrape.java
@@ -2,27 +2,35 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class dataScrape {
+    private static ChromeDriver driver;
 
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
     @Test
     public void openLandP() {
         String url = "https://www.cvmarket.lt/darbo-skelbimai?op=search&search%5Bjob_salary%5D=3&ga_track=homepage&search%5Blocations%5D%5B%5D=134&search%5Bcategories%5D%5B%5D=8&search%5Bkeyword%5D=";
-        ChromeDriver driver = new ChromeDriver();
         driver.get(url);
-        driver.manage().window().maximize();
-        driver.quit();
     }
-
     @Test
     public void scrape() {
         String url = "https://www.cvmarket.lt/darbo-skelbimai?op=search&search%5Bjob_salary%5D=3&search%5Blocations%5D%5B0%5D=134&search%5Bcategories%5D%5B0%5D=8&search%5Bkeyword%5D=&ga_track=homepage&start=";
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
         //Loop through every page
         for (int i = 0; i <= 201; i += 3) {
             String url1 = url + i + "0";
